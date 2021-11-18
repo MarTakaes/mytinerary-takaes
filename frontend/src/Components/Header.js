701a85336f9e22c4ada47535494da94529fe1191
MarTakaes <maaritakaes@gmail.com>
2021-11-18 00:46:16 -0300

    Feat: navbar upload

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './Nav'
+
+const HeaderHome = ()=>{
+
+    return (
+        <Nav />
+    )
+}
+
+export default HeaderHome
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderHome from "../Components/Header";
+
+export default class Home extends React.Component{
+
+    render (){
+        return (
+            <>
+                <HeaderHome />
+            </>
+        )
+    }
+}
