a861ffdb68389735c643f8619f238485b64ee348
MarTakaes <maaritakaes@gmail.com>
2022-01-05 18:18:45 -0300

    Feat:fixing servver

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-require('dotenv').config()
-const express = require("express")
-const cors = require("cors")
+// require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
 const Router = require('./routes/routes')
-require ('./config/database')
-const passport = require ('passport')
+const passport = require("passport")
+require('./config/database')
 const app = express()
-const path= require("path")
+
+require("./config/database");
+const path = require("path");
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+// middlewares
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 app.use(passport.initialize())
+app.use('/api', Router)
 
-app.use("/api", Router)
+const port = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.get("*", (req,res )=>{
-        res.sendFile(path.join(_dirname + "/client/build/index.html"))
-    })
-}
-app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', ()=> console.log(`Server is listening on port ${process.env.PORT || 4000}`))
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname + "/client/build/index.html"));
+//     });
+//   }
+
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`El server esta en el puerto ${port}`);
+  });
+  
